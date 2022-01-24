401bfec80c6341614028da3fe5ab3a252f55cab8
Renzo Visperas <renzovisperas07@gmail.com>
2022-01-24 20:39:23 +0800

    add rtk

--- src/Pages/Home/Sections/Work/Work.tsx
+++ src/Pages/Home/Sections/Work/Work.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import Title from "../../../../Components/Title/Title";
 import { Container, Box } from "@mui/material";
+import Paragraph from "../../../../Components/Paragraph/Paragraph";
 
 const Work = () => {
   return (
     <Container maxWidth="xl">
       <Box
-        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "10rem 0",
+          gap: "5rem",
+        }}
       >
-        <Title center>Work</Title>
+        <Title center>Projects</Title>
+        <Box></Box>
       </Box>
     </Container>
   );
