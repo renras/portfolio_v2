93c68e2eff5c04fe7c3932b2480e7992fb3a945c
Renzo Visperas <renzovisperas07@gmail.com>
2022-01-25 18:23:10 +0800

    refractor main.tsx

--- src/Pages/Home/Sections/Main/Main.tsx
+++ src/Pages/Home/Sections/Main/Main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Typography, Box, Button } from "@mui/material";
+import { Container, Typography, Box, Button, ButtonGroup } from "@mui/material";
 
 const Main = () => {
   const buttons: string[] = ["CONTACT ME", "CHECK MY WORK"];
@@ -8,47 +8,53 @@ const Main = () => {
     <Container
       maxWidth={false}
       sx={{
-        display: "flex",
-        alignItems: "center",
         minHeight: "100vh",
+        position: "relative",
       }}
     >
-      <Container maxWidth="md">
-        <Box sx={{ display: "flex", flexDirection: "column", gap: "5rem" }}>
-          <Box sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
-            <Typography
-              variant="h2"
-              component="h2"
-              sx={{ textAlign: "center" }}
-            >
-              Hello, I'm Ren and <br />
-              I'm a{" "}
-              <Typography variant="h2" component="span" color="primary">
-                Front End Developer
-              </Typography>
-            </Typography>
-            <Typography
-              variant="h5"
-              component="h3"
-              sx={{ marginTop: "1rem", textAlign: "center" }}
-            >
-              I focus on writing clean, elegant and efficient code.
-            </Typography>
-          </Box>
-          <Box sx={{ display: "flex", gap: "2rem", justifyContent: "center" }}>
-            {buttons.map((button, index) => {
-              return (
-                <Button
-                  size="large"
-                  variant={`${index == 0 ? "contained" : "outlined"}`}
-                  sx={{ fontWeight: "bold" }}
-                  key={index}
-                >
-                  {button}
-                </Button>
-              );
-            })}
-          </Box>
+      <Container
+        maxWidth="md"
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%,-50%)",
+        }}
+      >
+        <Typography variant="h2" component="h1" sx={{ textAlign: "center" }}>
+          Hello, I'm Ren and <br />
+          I'm a{" "}
+          <Typography variant="h2" component="span" color="primary">
+            Front End Developer
+          </Typography>
+        </Typography>
+        <Typography
+          variant="h5"
+          component="h2"
+          sx={{ marginTop: "2rem", textAlign: "center" }}
+        >
+          I focus on writing clean, elegant and efficient code.
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            gap: "2rem",
+            justifyContent: "center",
+            marginTop: "5rem",
+          }}
+        >
+          {buttons.map((button, index) => {
+            return (
+              <Button
+                size="large"
+                variant={`${index == 0 ? "contained" : "outlined"}`}
+                sx={{ fontWeight: "bold" }}
+                key={index}
+              >
+                {button}
+              </Button>
+            );
+          })}
         </Box>
       </Container>
     </Container>
