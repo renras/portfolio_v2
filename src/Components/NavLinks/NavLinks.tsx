21f67d34108d213bf7306264447d7bd81119eea6
Renzo Visperas <renzovisperas07@gmail.com>
2022-01-30 18:12:02 +0800

    replace scrollintoview function with react-scroll

--- src/Components/NavLinks/NavLinks.tsx
+++ src/Components/NavLinks/NavLinks.tsx
@@ -2,32 +2,17 @@ import React from "react";
 import { Box, Button } from "@mui/material";
 import { useAppDispatch } from "../../store/hooks";
 import { showDrawer } from "../../store/appSlice";
+import { Link } from "react-scroll";
 
 const NavLinks = ({ sx }: { sx?: Object }) => {
   const dispatch = useAppDispatch();
-  const buttons: string[] = ["About", "Projects", "Contact"];
+  const buttons: any[] = [
+    { name: "About", href: "aboutme" },
+    { name: "Projects", href: "projects" },
+    { name: "Contact", href: "footer" },
+  ];
 
-  const scrollIntoView = (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-    button: string
-  ): void => {
-    const aboutme =
-      document.querySelector("#aboutme").getBoundingClientRect().top +
-      window.scrollY;
-    const projects =
-      document.querySelector("#projects").getBoundingClientRect().top +
-      window.scrollY;
-    const footer =
-      document.querySelector("#footer").getBoundingClientRect().top +
-      window.scrollY;
-
-    if (button === "About")
-      window.scrollTo({ left: 0, top: aboutme, behavior: "smooth" });
-    if (button === "Projects")
-      window.scrollTo({ left: 0, top: projects, behavior: "smooth" });
-    if (button === "Contact")
-      window.scrollTo({ left: 0, top: footer, behavior: "smooth" });
-
+  const clickHandler = (): void => {
     dispatch(showDrawer(false));
   };
 
@@ -35,8 +20,15 @@ const NavLinks = ({ sx }: { sx?: Object }) => {
     <Box sx={sx}>
       {buttons.map((button, index) => {
         return (
-          <Button key={index} onClick={(e) => scrollIntoView(e, button)}>
-            {button}
+          <Button key={index}>
+            <Link
+              to={button.href}
+              smooth
+              duration={750}
+              onClick={() => clickHandler()}
+            >
+              {button.name}
+            </Link>
           </Button>
         );
       })}
